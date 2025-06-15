70b7a3182bfb4b5b88a8447222ca3c8ed9a0ea44
Léo <leoolivfriez@gmail.com>
2025-06-15 14:11:35 -0300

    Add files via upload

---
+++ Muscle Calculator.py
@@ -0,0 +1,57 @@
+VALORES = {
+    'PesoT' : '',
+    'PesoM' : '',
+    'Reps' : '',
+    'Calculo' : ''
+}
+
+def Calculos(PesoT, PesoM, Reps, Calculo):
+    if Calculo == 'M':
+        print('Insira o Peso Total:')
+        PesoT = float(input())
+        return PesoT / 2
+    elif Calculo == 'T':
+        print('Insira quanto tem em cada lado:')
+        PesoM = float(input())
+        return PesoM * 2
+    elif Calculo == 'V':
+        print('Insira quanto tem de Peso Total:')
+        PesoT = float(input())
+        print('Insira quantas repetições você fez:')
+        Reps = float(input())
+        return PesoT * Reps
+    else: 
+        return "continue"
+
+while True:
+    print(
+        "Escolha a função:\n"
+        "Para calcular quanto você deve colocar de cada lado, insira - M\n"
+        "Para calcular quanto de peso tem no total, insira - T\n"
+        "Para calcular qual o volume da série, insira - V\n"
+        "Para reinicar, insira - R"
+        )
+    Calculo = input().upper()
+    if Calculo == 'R':
+        break
+
+    result = Calculos("PesoT", "PesoM", "Reps", Calculo)
+
+    if result == 'continue':
+        print('Inválido')
+        continue
+    elif Calculo == 'M':
+        print(f'Você deve colocar {result} Kg em cada lado da barra') 
+    elif Calculo == 'T':
+        print(f'Tem {result} Kg na barra')
+    elif Calculo == 'V':
+        print(f'O volume da sua série foi de {result} Kg')
+    
+    print(
+          "Para sair, aperte - S\n"
+          "Para recomeçar, aperte qualquer outra letra\n"
+          )
+    escolha = input()
+    if escolha.upper() == 'S':
+        break
+    else: continue
